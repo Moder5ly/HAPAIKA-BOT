414af83c0946b0d9662e826261ae5b8a641ba051
Володимир Семендяєв <moder5ly@gmail.com>
2024-07-05 04:37:37 +0300

    Update changetime.py

--- shivu/modules/changetime.py
+++ shivu/modules/changetime.py
@@ -6,7 +6,6 @@ from pyrogram.types import Message
 
 ADMINS = [ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.OWNER]
 
-
 @shivuu.on_message(filters.command("changetime"))
 async def change_time(client: Client, message: Message):
     
@@ -14,30 +13,28 @@ async def change_time(client: Client, message: Message):
     chat_id = message.chat.id
     member = await shivuu.get_chat_member(chat_id,user_id)
         
-
     if member.status not in ADMINS :
-        await message.reply_text('⚠️ Ви не адміністратор цієї групи.')
+        await message.reply_text("❌️ Ви не адміністратор цієї групи, тому ця команда недоступна.")
         return
 
     try:
         args = message.command
         if len(args) != 2:
-            await message.reply_text('⚠️ Користуйтеся: <code>/changetime</code> <i>число</i>')
+            await message.reply_text("❌️ Некоректний формат! Формат: <code>/changetime</code> <i>число</i>")
             return
 
         new_frequency = int(args[1])
         if new_frequency < 10:
-            await message.reply_text('⚠️ Частота появи няшок має бути більша або дорівнювати 50.')
+            await message.reply_text("⚠️ Частота появи няшок має бути більша або дорівнювати 10.")
             return
-
     
         chat_frequency = await user_totals_collection.find_one_and_update(
             {'chat_id': str(chat_id)},
             {'$set': {'message_frequency': new_frequency}},
-            upsert=True,
-            return_document=ReturnDocument.AFTER
+            upsert = True,
+            return_document = ReturnDocument.AFTER
         )
 
         await message.reply_text(f'✅ Встановлено нову частоту появи няшок: {new_frequency} повідомлень.')
     except Exception as e:
-        await message.reply_text(f'❌ Не вийшло змінити частоту появи няшок. {str(e)}')
+        await message.reply_text(f'❌ Не вдалося змінити частоту появи няшок. Помилка: {str(e)}')
